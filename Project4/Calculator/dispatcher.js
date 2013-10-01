6b1caf97e78e50ecaa15f8f971ade6b3e010d2c4
Brandon Mills <mills.brandont@gmail.com>
2013-09-30 22:09:13 -0400

    Add pattern matching to dispatcher subscriptions

--- Project4/Calculator/dispatcher.js
+++ Project4/Calculator/dispatcher.js
@@ -6,39 +6,64 @@ var Dispatcher = function () {
 
 	var filters = [];
 
+	function match (pattern, candidate) {
+		for (var key in pattern) {
+			if (!pattern.hasOwnProperty(key)) continue;
+			if (!candidate.hasOwnProperty(key)) return false;
+			if (['string', 'number', 'boolean'].indexOf(typeof pattern[key]) >= 0) {
+				if (pattern[key] !== candidate[key]) return false;
+			} else if (pattern[key] instanceof RegExp
+			           && !pattern[key].test(candidate[key])) {
+				return false;
+			} else if (typeof pattern[key] === 'object'
+			           && typeof candidate[key] === 'object'
+			           && !match(pattern[key], candidate[key])) {
+				return false;
+			}
+		}
+		return true;
+	}
+
 	/*
 	 * Publish event e, with optional extra data.
 	 */
 	var pub = function (e, data) {
 		if (events[e]) {
-			events[e].forEach(function (cb) {
-				cb.call(null /* this value tbd */, data, e);
+			events[e].forEach(function (el) {
+				if (el.pattern == undefined || match(el.pattern, data))
+					el.callback.call(null /* this value tbd */, data, e);
 			});
 		}
-		filters.forEach(function (filter) {
-			if (filter.test(e)) {
-				filter.callback.call(null /* this value tbd */, data, e);
-			}
+		filters.forEach(function (el) {
+			if (el.test(e)
+			    && (el.pattern == undefined || match(el.pattern, data)))
+				el.callback.call(null /* this value tbd */, data, e);
 		});
 	};
 
 
 	/*
 	 * Subscribe to an event.
-	 * e is either a string literal, a RegExp, or a function returning bool.
+	 * e is either a string literal, or a RegExp.
+	 * pattern is an object pattern.
 	 */
-	var sub = function (e, cb) {
+	var sub = function (e, pattern, cb) {
+		if (arguments.length < 3) {
+			cb = pattern;
+			pattern = undefined;
+		}
+
 		if (typeof e === 'string') {
-			if (!Array.isArray(events[e])) {
-				events[e] = [cb];
-			} else {
-				events[e].push(cb);
-			}
-		} else if (typeof e === 'function' || e instanceof RegExp) {
+			if (!Array.isArray(events[e]))
+				events[e] = [];
+			events[e].push({
+				pattern: pattern,
+				callback: cb
+			});
+		} else if (e instanceof RegExp) {
 			filters.push({
-				test: e instanceof RegExp
-				      ? function () { return e.test.apply(e, arguments); }
-				      : e,
+				test: function () { return e.test.apply(e, arguments); },
+				pattern: pattern,
 				callback: cb
 			});
 		}
